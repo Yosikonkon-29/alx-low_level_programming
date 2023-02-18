4259b20ed4a2594f09b9cf298aec79e6cecec012
YOSI <oluwatoyosiolabamiji@gmail.com>
2023-02-18 15:19:02 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -8,4 +9,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
-
